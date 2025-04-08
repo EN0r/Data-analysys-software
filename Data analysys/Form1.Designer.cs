8cb4511fcdb8f90657c848ee781b64a0c3a6a156
rdv3736 <eriahnorwood99@gmail.com>
2025-04-08 01:00:42 +0100

    Add project files.

---
+++ Data analysys/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Data_analysys
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.filebrowser = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(12, 9);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(149, 31);
+            this.title.TabIndex = 0;
+            this.title.Text = "Data sorter";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(18, 48);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1226, 438);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "No file opened";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1205, 413);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // filebrowser
+            // 
+            this.filebrowser.Location = new System.Drawing.Point(167, 17);
+            this.filebrowser.Name = "filebrowser";
+            this.filebrowser.Size = new System.Drawing.Size(75, 23);
+            this.filebrowser.TabIndex = 0;
+            this.filebrowser.Text = "Open File";
+            this.filebrowser.UseVisualStyleBackColor = true;
+            this.filebrowser.Click += new System.EventHandler(this.filebrowser_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(15, 19);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(643, 404);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView2);
+            this.groupBox2.Location = new System.Drawing.Point(571, 519);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(673, 438);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Report Generated";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1256, 969);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.filebrowser);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.title);
+            this.Name = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button filebrowser;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Data analysys/Form1.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Office.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+namespace Data_analysys
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public DataTable createDTFromFile(string directory)
+        {
+            int rowCount = 0;
+            StreamReader reader = new StreamReader(directory);
+            string line = "";
+            DataTable dt = new DataTable();
+            while ((line = reader.ReadLine()) != null)
+            {
+                string[] tsv = line.Split(new char[] { '\t' }).ToArray();
+                //remove any end spaces from data
+                tsv = tsv.Select(x => x.Trim()).ToArray();
+
+                if (++rowCount == 1)
+                {
+                    foreach (string colName in tsv)
+                    {
+                        dt.Columns.Add(colName, typeof(string));
+                    }
+                }
+                else
+                {
+                    dt.Rows.Add(tsv);
+                }
+            }
+            return dt;
+        }
+        /*
+            Okay sooooo what i need to do is create a report function
+            to export a list of the most common countries that are on the list.
+            also the average price of whats spent per country.
+            Also the most common car rented based on their vehicle
+            
+            Problem statement:
+                The value numbers should be presented in GBP (I have included the values for conversion but have not completed this step.) 
+                Make sure the dates are displayed correctly (including times if you want to use them)
+                Use filters/slicers to allow users to interact with your findings
+                A final consideration would be to look closely at the Brokers. Enjoy does exist in this list. This means that our prices here are in a different category to others. How might you deal with that? 
+
+            Action plan:
+            Use streamlit to visualise this data and use this software as a backend tool to generate reports to be used by the frontend python software.
+            For the value numbers i will count the rows from the top of the colum with value and add a pound sign at the start of each using a for loop. Probably need to use a new thread for the operation for speed.
+         */
+        private void filebrowser_Click(object sender, EventArgs e)
+        {
+             OpenFileDialog fileDialog = new OpenFileDialog();
+
+            fileDialog.Filter = "TSV Files (*.tsv)|*.tsv";
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFilePath = fileDialog.FileName;
+                Console.WriteLine($"Selected File: {selectedFilePath}");
+                MessageBox.Show("File opened succesfully!");
+                groupBox1.Text = "Currently viewing: " + selectedFilePath;
+            }
+            var timer = new Stopwatch();
+
+            dataGridView1.DataSource = createDTFromFile(fileDialog.FileName);
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
